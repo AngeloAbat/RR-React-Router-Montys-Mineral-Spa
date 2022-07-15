90754a0fd3eec96a641f7757bf4c89f7c1511a4e
AngeloAbat <angeloabat25@gmail.com>
2022-07-14 19:23:10 -0700

    LateWork

---
+++ react-router/src/components/myAttempt.js
@@ -0,0 +1,8 @@
+export default function HotTempt () {
+    return (
+        <div className="burningHot">
+            <h1> Burning hot stove</h1>
+            <p>Learning Test, Attempt 1 worked, learning experience. 10/10 quick and easy</p>
+        </div>
+    )
+}
